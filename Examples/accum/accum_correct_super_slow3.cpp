f74a7c9fa5db3a3ba01beed3d91c1f219e5daafd
greg-stitt-uf <gstitt@ufl.edu>
2023-04-16 13:24:10 -0700

    Polished

--- Examples/accum/accum_correct_super_slow3.cpp
+++ Examples/accum/accum_correct_super_slow3.cpp
@@ -1,7 +1,7 @@
 // Greg Stitt
 // University of Florida
 //
-// accum_bad.cpp
+// accum_correct_super_slow2.cpp
 //
 // This SYCL program will create a parallel (vectorized) version of the following
 // sequential code:
@@ -16,104 +16,102 @@
 #include <vector>
 #include <random>
 #include <cmath>
+#include <chrono>
 
 #include <CL/sycl.hpp>
 
-const float ALLOWABLE_ERROR = 0.000001;
-bool are_floats_equal(float a, float b, float abs_tol=ALLOWABLE_ERROR, float rel_tol=ALLOWABLE_ERROR) {
+class accum;
 
-  float diff = fabs(a-b);
-  return (diff <= abs_tol || diff <= rel_tol * fmax(fabs(a), fabs(b)));
+void print_usage(const std::string& name) {
+  std::cout << "Usage: " << name << " vector_size (must be positive)" << std::endl;      
 }
 
 
-class accum;
-
 int main(int argc, char* argv[]) { 
 
+  // Check correct usage of command line.
   if (argc != 2) {
-    std::cout << "Usage: " << argv[0] << " vector_size" << std::endl;
-    return 1;
+    print_usage(argv[0]);
+    return 1;    
   }
 
-  const unsigned VECTOR_SIZE = atoi(argv[1]);
-  unsigned num_work_items = ceil(VECTOR_SIZE / 2.0);
+  // Get vector size from command line.
+  int vector_size;
+  vector_size = atoi(argv[1]);
 
-  //std::cout << "NUM_WORK_ITMES = " << num_work_items << std::endl;
-  //for (int i=VECTOR_SIZE; i > 1; i = ceil(i/2.0))
-  //  std::cout << i << std::endl;
-  
-  
-  std::vector<int> x_h(VECTOR_SIZE);
-  std::vector<int> y_h(VECTOR_SIZE);
+  if (vector_size <= 0) {
+    print_usage(argv[0]);    
+    return 1;    
+  }
+
+  std::chrono::time_point<std::chrono::system_clock> start_time, end_time;
+  unsigned num_work_items = ceil(vector_size / 2.0);
+  std::vector<int> x_h(vector_size);
+  std::vector<int> y_h(vector_size);
   int correct_out = 0;
 
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_int_distribution<> dist(-10, 10);
 
-  for (size_t i=0; i < VECTOR_SIZE; i++) {
-    //x_h[i] = dist(gen);
-    x_h[i] = i % 10;
+  for (size_t i=0; i < vector_size; i++) {
+    x_h[i] = dist(gen);
     correct_out += x_h[i];
   }
-
-  /*  for (int i=0; i < VECTOR_SIZE; i++) {
-    std::cout << x_h[i] << std::endl;
-  }
-  std::cout << std::endl;*/
   
   try {
     cl::sycl::queue queue(cl::sycl::default_selector_v, [] (sycl::exception_list el) {
-       for (auto ex : el) { std::rethrow_exception(ex); }
-    } );
-   
+	for (auto ex : el) { std::rethrow_exception(ex); }
+      } );
 
-    int iteration = 0;
-    for (int size = VECTOR_SIZE; size > 1; size = ceil(size / 2.0), iteration++) {
-      int stride = pow(2, iteration);
-      //      std::cout << "Stride: " << stride << std::endl;
+    start_time = std::chrono::system_clock::now();
 
-      {
-	cl::sycl::buffer<int, 1> x_buf {x_h.data(), cl::sycl::range<1>(VECTOR_SIZE) };
-	cl::sycl::buffer<int, 1> y_buf {y_h.data(), cl::sycl::range<1>(VECTOR_SIZE) };
+    for (int size = vector_size; size > 1; size = ceil(size / 2.0)) {
+
+      cl::sycl::buffer<int, 1> x_buf {x_h.data(), cl::sycl::range<1>(size) };
+      cl::sycl::buffer<int, 1> y_buf {y_h.data(), cl::sycl::range<1>(ceil(size/2.0)) };
       
-	queue.submit([&](cl::sycl::handler& handler) {
-
-	    cl::sycl::accessor x_d(x_buf, handler, cl::sycl::read_only);
-	    cl::sycl::accessor y_d(y_buf, handler, cl::sycl::write_only);
-
-	    handler.parallel_for<class accum>(cl::sycl::range<1> { num_work_items }, [=](cl::sycl::id<1> i) {
-
-		int base = 2*stride*i;
-		if (2*i + 1 < size)
-		  y_d[base] = x_d[base] + x_d[base + stride];
-		else if (2*i + 1 == size)
-		  y_d[base] = x_d[base];
-	      });
-	  });
-
-	queue.wait();
-      }
+      queue.submit([&](cl::sycl::handler& handler) {
+	  
+	  cl::sycl::accessor x_d(x_buf, handler, cl::sycl::read_only);
+	  cl::sycl::accessor y_d(y_buf, handler, cl::sycl::write_only);
+	  
+	  handler.parallel_for<class accum>(cl::sycl::range<1> { num_work_items }, [=](cl::sycl::id<1> i) {
+	      
+	      if (2*i + 1 == size)
+		y_d[i] = x_d[2*i];
+	      else if (2*i + 1 < size)
+		y_d[i] = x_d[2*i] + x_d[2*i+1];
+	    });
+	});
       
-      /*for (int i=0; i < VECTOR_SIZE; i++) {
-	std::cout << y_h[i] << std::endl;
-      }
-      std::cout << std::endl;*/
+      queue.wait();      
+      y_buf.get_access<cl::sycl::access::mode::read>();
 
-      x_h = y_h;
+      // CHANGES FROM PREVIOUS EXAMPLE
+      // Previously we copied the entire output vector
+      // back to the input vector. This was very wasteful
+      // because most iterations have few valid outputs. In
+      // this version we optimize the code to only copy back
+      // the actual outputs.
+      for (int i=0; i < ceil(size/2.0); i++) {
+	x_h[i] = y_h[i];
+      }
     }
+    
+    end_time = std::chrono::system_clock::now();
   }
   catch (cl::sycl::exception& e) {
     std::cout << e.what() << std::endl;
     return 1;
   }
 
-  if (!are_floats_equal(correct_out, x_h[0])) {
+  if (correct_out != x_h[0]) {
     std::cout << "ERROR: output was " << x_h[0] << " instead of " << correct_out << std::endl;
     return 1;
   }
 
-  std::cout << "SUCCESS!" << std::endl;
+  std::chrono::duration<double> seconds = end_time - start_time;
+  std::cout << "SUCCESS! Time: " << seconds.count() << "s" << std::endl;
   return 0;
 }
