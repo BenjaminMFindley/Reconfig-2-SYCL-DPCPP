f9da3e1882e9639eb6e1b9ac00e6cf857f580359
greg-stitt-uf <gstitt@ufl.edu>
2023-04-17 12:18:10 -0700

    Added explicit transfer option

--- Examples/vector_addition/vector_add1.cpp
+++ Examples/vector_addition/vector_add1.cpp
@@ -45,29 +45,38 @@ int main(int argc, char* argv[]) {
   // we can add new scope for all the device code. It will be explained in detail later,
   // but the previous problem was that the output was never transferred from device memory
   // back to the host. By putting the device code in a separate scope, all objects are
-  // "destructed", which for out_buf causes the host to transfer the outputs back from the
-  // device. There are other methods we could have use too, but using a separate scope
-  // is considered a good practice.
+  // "destructed" at the end of the scope, which for out_buf causes the host to transfer
+  // the outputs back from the device. There are other methods we could have used,
+  // but using a separate scope is common, especially since that scope is usually created
+  // by a try-catch, as we will see in the next example.
   {
     cl::sycl::buffer<int, 1> in1_buf {in1_h.data(), cl::sycl::range<1>(in1_h.size()) };
     cl::sycl::buffer<int, 1> in2_buf {in2_h.data(), cl::sycl::range<1>(in2_h.size()) };
     cl::sycl::buffer<int, 1> out_buf {out_h.data(), cl::sycl::range<1>(out_h.size()) };
     
     queue.submit([&](cl::sycl::handler& handler) {
-
+	
 	cl::sycl::accessor in1_d(in1_buf, handler, cl::sycl::read_only);
 	cl::sycl::accessor in2_d(in2_buf, handler, cl::sycl::read_only);
 	cl::sycl::accessor out_d(out_buf, handler, cl::sycl::write_only);
-
+	
 	handler.parallel_for<class vector_add>(cl::sycl::range<1> { in1_h.size() }, [=](cl::sycl::id<1> i) {
 	    out_d[i] = in1_d[i] + in2_d[i];
 	  });
-
+	
       });
-
+    
     queue.wait();
+    
   }
   
+  // CHANGES FROM PREVIOUS EXAMPLE
+  // As an alternative to adding a new scope to ensure the output is transferred
+  // back to the host, we can explicitly transfer the outputs with the following
+  // code, where the host requests read access to the output buffer.
+  // 
+  // out_buf.get_access<cl::sycl::access::mode::read>();
+    
   std::cout << "Operation complete:\n"
 	    << "[" << in1_h[0] << "] + [" << in2_h[0] << "] = [" << out_h[0] << "]\n"
 	    << "[" << in1_h[1] << "] + [" << in2_h[1] << "] = [" << out_h[1] << "]\n"
